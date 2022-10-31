df8b1786c0cadf288414f971718242aa185dd9bc
Haiwenlei <haiwenlei@gmail.com>
2022-10-31 00:35:03 -0400

    Getting census data through APT

---
+++ scripts/API_Census.R
@@ -0,0 +1,78 @@
+
+
+# setup -------------------------------------------------------------------
+
+library(tidycensus)
+library(tidyverse)
+library(tmap)
+library(sf)
+
+
+# Request an api key from https://api.census.gov/data/key_signup.html
+
+# Install your census key:
+
+tidycensus::census_api_key(
+  # Paste your key here!,
+  install = TRUE)
+
+# Get census variables associated with the annual American Community Survey 
+# (ACS):
+
+census_vars <-
+  tidycensus::load_variables(
+    year = 2021,
+    dataset = 'acs1')
+
+
+
+# finding variables  ------------------------------------------------------
+
+
+
+# Look for variables 
+
+census_vars %>% 
+  filter(str_detect(concept, 'POPULATION')) %>% 
+  select(concept) %>% 
+  distinct()
+
+# get the variable id for median monthly housing costs 
+
+census_vars %>%   
+  filter(concept == 'TOTAL POPULATION') %>% 
+  pull(name)
+
+
+
+# Get ACS data ------------------------------------------------------------
+
+# ACS 2020 - 2015 population data data census tract level  
+
+acs_population_2020 <-
+  tidycensus::get_acs(
+    geography = 'tract',
+    variables = 'B01003_001',
+    year = 2020,
+    state = '06', # 06 represent CA
+    survey = 'acs5',
+    geometry = TRUE) %>% 
+  st_make_valid()
+
+tmap_mode('view')
+
+tm_shape(acs_population_2020) + 
+  tm_polygons(col = 'estimate',
+              title = 'observations',
+              breaks = 
+                c(2000,
+                  3000,
+                  4000,
+                  5000,
+                  10000,
+                  15000,
+                  20000, 
+                  25000,
+                  30000,
+                  35000,
+                  40000))
