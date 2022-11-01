dc5b28c715f512e462436bac257ba89393515374
Jingsong Gao <jg2109@georgetown.edu>
2022-11-01 18:09:59 -0400

    Refactor data prepare script, replace dataset.

--- scripts/data_prepare.R
+++ scripts/data_prepare.R
@@ -1,11 +1,12 @@
 
 # Setup -------------------------------------------------------------------
 
-library(tidycensus)
+library(purrr)
 library(tidyverse)
 library(sf)
+library(stars)
 library(tmap)
-library(purrr)
+
 
 # Download ----------------------------------------------------------------
 
@@ -85,6 +86,7 @@ walk2(
   calveg_urls,
   ~ download.file(.y, .x))
 
+
 # Unzip files -------------------------------------------------------------
 
 filenames %>%
@@ -113,130 +115,301 @@ calveg_filenames %>%
       junkpaths = TRUE,
       exdir = gsub(pattern = "\\.zip$", "", .x)))
 
+rm(filenames, urls)
+
+
 # Exploring shape files --------------------------------------------------------------
 
 # Many of our raw datasets are so big which prevents us from
-# using purrr:map() function. We have to read, process, extract
-# meta info one-by-one to save memory.
-
-# Also, to extract meta info, we have to define a function here
-
-extract_metainfo <-
-  function(filename, url, shp, desp) {
-    bbox <- st_bbox(shp)
-    meta <- list(
-      file_name = filename,
-      online_link = url,
-      file_size_mb = file.size(filename) / 1e6,
-      crs_epsg = st_crs(shp)$epsg,
-      number_of_fields_or_layers = ncol(shp),
-      number_of_features_or_cells = nrow(shp),
-      extent_xmin = bbox$xmin,
-      extent_xmax = bbox$xmax,
-      extent_ymin = bbox$ymin,
-      extent_ymax = bbox$ymax,
-      description_of_data = desp)
-    return(meta)
-  }
+# using purrr:map() function. We have to read, process one-by-one
+# to save memory. Also, raw shapefiles won't be stored in global
+# variables in order to save memory.
 
 
-# Read cal fire data
+# California County -------------------------------------------------------
 
-cal_fire <-
-  st_read("data/raw/shapefiles/cal_fire_all.geojson")
+cal_counties <-
+  st_read("data/raw/shapefiles/cal_counties") %>%
+  st_make_valid() %>%
+  st_transform(4326)
 
-cal_fire_meta <-
-  extract_metainfo(
-    "data/raw/shapefiles/cal_fire_all.geojson",
-    urls$calfire,
-    cal_fire,
-    "California fires record (as ploygons, with attributes like year, cause).")
+cal_counties %>%
+  set_names(
+    tolower(
+      names(.))) %>%
+  st_write(
+    dsn = paste0("data/processed/cal_counties.geojson"),
+    delete_dsn = TRUE)
+
+
+# California Fire ---------------------------------------------------------
 
 # Make polygon valid and save as processed data
 
+cal_fire <-
+  st_read("data/raw/shapefiles/cal_fire_all.geojson") %>%
+  st_make_valid() %>%
+  st_transform(4326) %>%
+  set_names(
+    tolower(
+      names(.))) %>%
+  select(alarm_date,
+         cont_date,
+         cause,
+         gis_acres,
+         objective,
+         geometry)
+
+cal_fire_r <-
+  cal_fire %>%
+  filter(alarm_date > '2010-01-01') %>%
+  select(geometry) %>%
+  mutate(n = 1) %>%
+  st_rasterize(cal_stars,
+             options = c("MERGE_ALG=ADD",
+                         "ALL_TOUCHED=TRUE")) %>%
+  replace(. == 0, NA)
+
+(cal_fire_r / 1000) %>%
+  tm_shape() +
+  tm_raster(col = "n",
+            alpha = 0.8,
+            palette = "YlOrRd",
+            style = "cont")
+
+# Save raster data
+
+write_stars(cal_fire_r / 1000, "data/processed/cal_fire.tif")
+
+# Save shapefile
+
 cal_fire %>%
-  st_make_valid() %>%
-  st_transform(4326) %>%
   st_write(
-    dsn = paste0("data/processed/cal_fire_all.geojson"),
+    dsn = "data/processed/cal_fire_all.geojson",
     delete_dsn = TRUE)
 
-# Remove from memory
-rm(cal_fire)
+# Release memory
 
-# Read ca building
+rm(cal_fire, cal_fire_r)
 
-cal_building <-
-  st_read("data/raw/shapefiles/cal_building.geojson")
 
-cal_building_meta <-
-  extract_metainfo(
-    "data/raw/shapefiles/cal_building.geojson",
-    urls$building,
-    cal_building,
-    "California building shapefile (as ploygons). We only use it as point to calculate building density.")
+# California Building -----------------------------------------------------
 
-# Convert polygons to centroid
+# Bbox for rasterize
 
-cal_building_point <-
-  cal_building %>%
-  st_make_valid() %>%
-  st_centroid()
+cal_stars <-
+  st_as_stars(
+    st_bbox(
+      cal_counties %>%
+        st_transform(4326)),
+    nx = 800,
+    ny = 800,
+    values = 0)
 
-cal_building_point %>%
-  select(-capture_dates_range) %>%
+# Rasterize building, count all points in a grid.
+
+cal_building_r <-
+  st_read("data/raw/shapefiles/cal_building.geojson") %>%
+  select(geometry) %>%
+  mutate(n = 1) %>%
   st_transform(4326) %>%
-  st_write(
-    dsn = paste0("data/processed/cal_building.geojson"),
-    delete_dsn = TRUE)
-  
+  st_rasterize(cal_stars,
+               options = c("MERGE_ALG=ADD",
+                           "ALL_TOUCHED=TRUE")) %>%
+  replace(. == 0, NA)
+
+# A quick viz
+
+tmap_mode("view")
+
+(cal_building_r / 1000) %>%
+  tm_shape() +
+  tm_raster(col = "n",
+            alpha = 0.8,
+            palette = "Greys",
+            style = "cont")
+
+# Save raster data
+
+write_stars(cal_building_r / 1000, "data/processed/cal_building.tif")
 
 # Remove from memory
 
-rm(cal_bulding)
+rm(cal_building_r)
 
-all_filenames <-
-  c("data/raw/shapefiles/cal_fire_all.geojson",
-    "data/raw/shapefiles/cal_building.geojson",
-    "data/raw/shapefiles/cal_counties",
-    calveg_filenames)
 
-# Meta info dataframe
+# California Vegetation ---------------------------------------------------
 
-meta <-
-  tibble(
-    file_name = character(),
-    online_link = character(),
-    file_size_mb = character(),
-    crs_epsg = character(),
-    number_of_fields_or_layers = numeric(),
-    number_of_features_or_cells = numeric(),
-    extent_xmin = numeric(),
-    extent_xmax = numeric(),
-    extent_ymin = numeric(),
-    extent_ymax = numeric(),
-    description_of_data = character())
-shapes <-
-  all_filenames %>%
+calveg <-
+  calveg_filenames %>%
   map(
-    ~ st_read(.x) %>%
-      set_names(
-        tolower(
-          names(.))))
+    ~ gsub(pattern = "\\.zip$", "", .x) %>%
+      st_read() %>%
+      
+      # Calculate tree coverage area
+      
+      mutate(
+        TREE_CFA = as.numeric(TREE_CFA_CLASS) * SHAPE_Area / 100) %>%
+      
+      # Drop na
+      
+      drop_na(TREE_CFA) %>%
+      
+      # Filter geometry and tree coverage area
+      
+      select(SHAPE, TREE_CFA) %>%
+      
+      # Transform crs
+      
+      st_transform(4326) %>%
+      
+      # Rasterize
+      
+      st_rasterize(cal_stars,
+                   options = c("MERGE_ALG=ADD",
+                               "ALL_TOUCHED=TRUE")))
+
+# Combine rasters from different regions
+
+calveg_r <-
+  calveg %>%
+  reduce(c) %>%
+  merge() %>%
+  st_apply(
+    1:2,
+    sum,
+    .fname = "tree_area") %>%
+  replace(. == 0, NA)
+
+# A quick viz
+
+calveg_r %>%
+  tm_shape() +
+  tm_raster(col = "tree_area",
+            alpha = 0.8,
+            palette = "Greens",
+            style = "cont")
+
+# Save raster data
+
+write_stars(calveg_r, "data/processed/calveg.tif")
+
+# Release memory
+
+rm(calveg,
+   calveg_r,
+   calveg_names,
+   calveg_filenames,
+   calveg_urls)
+
+rm(cal_stars,
+   cal_counties)
+
+
+# Test reload -------------------------------------------------------------
+
+# Read back some raster data
+
+r <-
+  list(
+    veg = read_stars("data/processed/calveg.tif"),
+    building = read_stars("data/processed/cal_building.tif"),
+    fire = read_stars("data/processed/cal_fire.tif"))
+
+# Plot in one
+
+tm_shape(r$veg) +
+  tm_raster(alpha = 0.8, palette = "Greens", style = "cont", contrast = c(0.2, 0.9)) +
   
-# Check validity
+  tm_shape(r$building) +
+  tm_raster(alpha = 0.8, palette = "Greys", style = "cont", contrast = c(0.2, 0.9)) +
+  
+  tm_shape(r$fire) +
+  tm_raster(alpha = 0.8, palette = "YlOrRd", style = "cont", contrast = c(0.2, 0.9))
 
-shapes %>%
+# Plot some correlation
+
+r_tbl <-
+  r %>%
   map(
-    ~ filter(
-      .,
-      !st_is_valid(.)) %>%
-      nrow())
+    ~ .x %>%
+      as_tibble() %>%
+      set_names("x", "y", "value") %>%
+      pull(value) %>%
+      replace_na(0)) %>%
+  as_tibble()
 
-# heck crs
+r_tbl %>%
+  select(veg, building) %>%
+  filter(veg != 0 | building != 0) %>%
+  ggplot() +
+  geom_point(aes(x = veg, y = building))
 
-shapes %>%
-  map(
-    ~ st_crs(.x))
+r_tbl %>%
+  select(veg, fire) %>%
+  filter(fire != 0) %>%
+  ggplot() +
+  geom_point(aes(x = veg, y = fire))
+
+r_tbl %>%
+  select(building, fire) %>%
+  filter(fire != 0) %>%
+  ggplot() +
+  geom_point(aes(x = building, y = fire))
+
+rm(r, r_tbl)
+
+# Code domain mapping -----------------------------------------------------
+
+# Fire cause, from https://frap.fire.ca.gov/frap-projects/fire-perimeters/
+
+fire_cause <-
+  list(
+    
+    # Human readable names for fire causes
+    
+    cause_name = list(
+      `1` = "Lightning",
+      `2` = "Equipment Use",
+      `3` = "Smoking",
+      `4` = "Campfire",
+      `5` = "Debris",
+      `6` = "Railroad",
+      `7` = "Arson",
+      `8` = "Playing with fire",
+      `9` = "Miscellaneous",
+      `10` = "Vehicle",
+      `11` = "Powerline",
+      `12` = "Firefighter Training",
+      `13` = "Non-Firefighter Training",
+      `14` = "Unknown / Unidentified",
+      `15` = "Structure",
+      `16` = "Aircraft",
+      `17` = "Volcanic",  # Not appeared in data
+      `18` = "Escaped Prescribed Burn",
+      `19` = "Illegal Alien Campfire"),
+    
+    # Category of fire causes
+    # `human`: Caused by human, but vehicle/structure are not include.
+    # `natural`: Caused by natural phenomenons, like lightning.
+    # `vehicle`: Caused by vehicle, perhaps car spontaneous combustion.
+    # `structure`: Caused by static structures, like power line.
+    # `other`: Cause is not easy to classify into any categories above.
+    
+    category = list(
+      human = c(2, 3, 4, 6, 7, 8, 19),
+      natural = c(1, 17, 18),
+      vehicle = c(10, 16),
+      structure = c(11, 15),
+      other = c(5, 9, 12, 13, 14)))
+
+saveRDS(fire_cause, "data/processed/fire_cause.rds")
+
+rm(fire_cause)
 
 
+# Clean folder ------------------------------------------------------------
+
+# To save storage, we remove all raw data since they no longer needed.
+
+unlink("data/raw/shapefiles/[!.]*", recursive = TRUE)
---
+++ scripts/metadata.R
@@ -0,0 +1,111 @@
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+library(sf)
+library(stars)
+
+
+# Extract meta information ------------------------------------------------
+
+extract_metainfo <-
+  function(filename, url, desp) {
+    
+    # Read in data
+    
+    if (endsWith(filename, ".tif")) {
+      shp <- read_stars(filename)
+    }
+    else {
+      shp <- st_read(filename)
+    }
+    
+    # Get bbox
+    
+    bbox <- st_bbox(shp)
+    
+    # Get num of feature and num of layer
+    
+    if (is(shp, "sf")) {
+      # shapefile
+      nlayer = ncol(shp)
+      nfeat = nrow(shp)
+    }
+    if (is(shp, "stars")) {
+      # raster data
+      nlayer = length(shp)
+      nfeat = nrow(shp) * ncol(shp)
+    }
+    
+    meta <- tibble(
+      file_name = basename(filename),
+      online_link = url,
+      file_size_mb = file.size(filename) / 1e6,
+      crs_epsg = st_crs(shp)$epsg,
+      number_of_fields_or_layers = nlayer,
+      number_of_features_or_cells = nfeat,
+      extent_xmin = bbox$xmin,
+      extent_xmax = bbox$xmax,
+      extent_ymin = bbox$ymin,
+      extent_ymax = bbox$ymax,
+      description_of_data = desp)
+    
+    return(meta)
+  }
+
+
+cal_fire_meta <-
+  extract_metainfo(
+    "data/processed/cal_fire_all.geojson",
+    "https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-all-1.geojson",
+    "California fires record (as ploygons). We select some interested attributes
+    including date, cause, burned area, etc.")
+
+cal_counties_meta <-
+  extract_metainfo(
+    "data/processed/cal_counties.geojson",
+    "https://data.ca.gov/dataset/e212e397-1277-4df3-8c22-40721b095f33/resource/b0007416-a325-4777-9295-368ea6b710e6/download/ca-county-boundaries.zip",
+    "California county boundary shapefile.")
+
+cal_building_meta <-
+  extract_metainfo(
+    "data/processed/cal_building.tif",
+    "https://usbuildingdata.blob.core.windows.net/usbuildings-v2/California.geojson.zip",
+    "California building density (raster, unit: 1000 building/pixel). We use Microsoft's USBuildingFootPrints
+    dataset (which contains more than 11 million building polygons) to create this
+    raster data. The raw data is too large (3.3 GB), so we only report meta info
+    of this raster data.")
+
+cal_veg_meta <-
+  extract_metainfo(
+    "data/processed/calveg.tif",
+    "https://data.fs.usda.gov/geodata/edw/edw_resources/fc/S_USA.EVMid_R05_CentralCoast.gdb.zip",
+    "California tree coverage area (raster). We use CalVeg dataset (which contains
+    about 300,000 polygons of vegetation classification records) to create this
+    raster data. The raw data is separated in 11 sub-regions and the total size
+    is too large(~ 3 GB), so we only report meta info of this raster data.")
+  
+cal_pop_meta <-
+  extract_metainfo(
+    "data/processed/cal_population_tract.geojson",
+    "https://www.census.gov/programs-surveys/acs",
+    "California population data (polygons) from Census ACS. We calculate population
+    density based on the area of polygons.")
+
+meta_tbl <-
+  bind_rows(
+    cal_fire_meta,
+    cal_counties_meta,
+    cal_building_meta,
+    cal_veg_meta,
+    cal_pop_meta)
+
+meta_tbl %>%
+  write_csv("data/metadata.csv")
+
+rm(cal_fire_meta,
+   cal_counties_meta,
+   cal_building_meta,
+   cal_veg_meta,
+   cal_pop_meta,
+   meta_tbl)
