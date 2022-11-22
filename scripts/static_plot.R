43b0fb5740f50c0347ce00cb4b49d12076e34c16
Jingsong Gao <512320254@qq.com>
2022-11-21 21:09:55 -0500

    Add some static plots.

--- scripts/static_plot.R
+++ scripts/static_plot.R
@@ -41,6 +41,11 @@ fire_with_cause <-
       set_units("km"))
 
 
+ourtheme <-
+  theme_fivethirtyeight() +
+  theme(
+    axis.title = element_text())
+
 # Plot of fire vs year ----------------------------------------------------
 
 fire_with_cause %>%
@@ -64,17 +69,23 @@ fire_with_cause %>%
   ggplot(aes(x = year, y = n)) +
   geom_point() +
   geom_smooth(method = "lm") +
-  theme_fivethirtyeight() +
   
   # Facet by cause
   
   facet_wrap(
     ~ cause_category,
     scales = "free_y") +
+  
+  ourtheme +
+  
   labs(x = "Year",
        y = "Number of fires",
        title = "Wildfires become more frequent in recent years?",
        subtitle = "A scatterplot of fire count per year by cause")
+
+ggsave("output/plots/fire_year.png",
+       height = 8,
+       width = 8)
 
 
 # Plot of distance to road ------------------------------------------------
@@ -89,12 +100,69 @@ fire_with_cause %>%
   
   ggplot(aes(x = distance)) +
   geom_histogram(color = "black", fill = "white") +
-  theme_fivethirtyeight() +
   
   facet_wrap(
     ~ cause_category,
     scales = "free_y") +
+  
+  ourtheme +
+  
   labs(x = "Distance to road (highway/railway)",
        y = "Number of fires",
        title = "Wildfires occur more often close to roads?",
        subtitle = "A histogram of fire and distance to road by cause")
+
+ggsave("output/plots/fire_road_distance.png",
+       height = 8,
+       width = 8)
+
+
+# Calendar heatmap --------------------------------------------------------
+
+fire_with_cause %>%
+  as_tibble() %>%
+  
+  filter(
+    cause_category != "other") %>%
+  
+  mutate(
+    year = year(alarm_date),
+    week = week(alarm_date)) %>%
+  
+  filter(year >= 1980 & year <= 2020) %>%
+  
+  group_by(year, week, cause_category) %>%
+  
+  summarise(n = n()) %>%
+  
+  mutate(
+    level = cut(
+      n,
+      breaks = c(-Inf, 5, 10, 20, 50, Inf),
+      include.lowest = TRUE,
+      right = TRUE,
+      labels = c("Very Low",
+                 "Low",
+                 "Medium",
+                 "High",
+                 "Very High"))) %>%
+  
+  ggplot() +
+  geom_tile(aes(x = week, y = year, fill = level)) +
+  scale_fill_brewer(palette = "OrRd") +
+  
+  facet_wrap(
+    ~ cause_category) +
+  
+  ourtheme +
+  
+  labs(x = "Week of Year",
+       y = "Year",
+       title = "When did wildfires happen?",
+       subtitle = "A calendar heatmap of fire count each week in 1980-2020",
+       caption = "Very Low: 0~5, Low: 6~10,\nMedium: 11~20, High: 21~50,\nVery High: 50+",
+       fill = "Frequency")
+
+ggsave("output/plots/fire_week.png",
+       height = 8,
+       width = 8)
