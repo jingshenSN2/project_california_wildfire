d9f2de63214efc008871ec20903bfe6717de018f
Jingsong Gao <jg2109@georgetown.edu>
2022-12-06 17:00:29 -0500

    Add pack shinyapp only option.

--- pack.sh
+++ pack.sh
@@ -16,7 +16,21 @@ else
     exit 1
 fi
 
-# Create a zip file with the name
-git archive --format zip --output gis_project_$name.zip HEAD
+# Allow user to select: (1) for whole project (2) for shiny app only
+echo "Please select the directory to pack (1) whole project (2) shiny"
+read pack_dir
 
-echo "Saved as gis_project_$name.zip"
+# If 1, name will be gao_jingsong, 2 will be lei_haiwen
+if [ $pack_dir -eq 1 ]; then
+    # Create a zip file with the name
+    git archive --format zip --output gis_project_$name.zip HEAD
+    echo "Saved as gis_project_$name.zip"
+elif [ $pack_dir -eq 2 ]; then
+    # Pack only shiny app
+    git archive --format zip \
+      --output gis_project_shiny_app_$name.zip HEAD:output/calfire_app
+    echo "Saved as gis_project_shiny_app_$name.zip"
+else
+    echo "Invalid input"
+    exit 1
+fi
